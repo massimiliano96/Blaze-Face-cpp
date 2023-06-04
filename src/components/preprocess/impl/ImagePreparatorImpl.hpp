bfeae72d51757c9606d8b4d6f2c6d8ea6e60abdb
Massimiliano Riva <massimilianoriva96@gmail.com>
2023-06-04 18:13:03 +0200

    refactoring: Image preproceess

---
+++ src/components/preprocess/impl/ImagePreparatorImpl.hpp
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "components/preprocess/ImagePreparator.hpp"
+
+class ImagePreparatorImpl : public ImagePreparator
+{
+public:
+    ImagePreparatorImpl(int width, int height);
+    cv::Mat prepare(const cv::Mat image) const override;
+
+private:
+    int inputWidth;
+    int inputHeight;
+};
